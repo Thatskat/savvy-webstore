5e820242adba915e708c87f1c5843e17ba085578
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-17 12:02:35 +1000

    finished static page routing

--- client/savvy-webstore/src/App.jsx
+++ client/savvy-webstore/src/App.jsx
@@ -4,14 +4,16 @@ import Layout from "./components/layout/Layout";
 // PAGES IMPORTS
 import HomePage from "./pages/Home";
 import CartPage from "./pages/Cart";
+import WishlistPage from './pages/Wishlist'
 
 // STATIC PAGES
 import StoresPage from "./pages/Stores";
 import CareersPage from "./pages/Careers";
 import AboutUsPage from "./pages/AboutUs";
-import ReturnsPage from './pages/Returns';
+import ReturnsPage from "./pages/Returns";
 import ContactUsPage from "./pages/ContactUs";
 import PrivacyPolicyPage from "./pages/PrivacyPolicy";
+import TermsConditionsPage from "./pages/TermsConditions";
 
 // SHOP PAGES - MAIN
 import ShopPage from "./pages/Shop";
@@ -27,14 +29,17 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        {/* CART AND WISHLIST */}
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/wishlist" element={<WishlistPage />}/>
         {/* STATIC PAGES */}
         <Route path="/stores" element={<StoresPage />} />
         <Route path="/careers" element={<CareersPage />} />
-        <Route path="/about" element={<AboutUsPage />}/>
-        <Route path="/returns" element={<ReturnsPage />}/>
-        <Route path="/contact" element={<ContactUsPage />}/>
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />}/>
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/returns" element={<ReturnsPage />} />
+        <Route path="/contact" element={<ContactUsPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/terms-conditions" element={<TermsConditionsPage />} />
         {/* SHOP PAGES - MAIN */}
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/shop/womens" element={<WomensPage />} />
