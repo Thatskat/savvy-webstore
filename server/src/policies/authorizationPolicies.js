f181be2a25f01bbec43218cf4878513a35aea861
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-18 19:54:14 +1000

    started the set up of the authorizedRoutes

---
+++ server/src/policies/authorizationPolicies.js
@@ -0,0 +1,8 @@
+const Joi = require("joi");
+const ErrorsApi = require("../utilities/ErrorsApi");
+
+module.exports = {
+    validateAuthorization(req, res, next) {
+        const schema = Joi.object
+    }
+}
--- server/src/routes/authorizedRoutes.js
+++ server/src/routes/authorizedRoutes.js
@@ -1,1 +1,7 @@
-cob
+const express = require("express");
+const router = express.Router();
+
+
+module.exports = () => {
+    
+}
