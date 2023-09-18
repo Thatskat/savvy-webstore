6f5ced55da95db718a7c055c497b6136109311f6
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-18 19:49:34 +1000

    set up routes.js

---
+++ server/src/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const authorizedRoutes = require("./authorizedRoutes");
+
+module.exports = () => {
+  router.get("/", (req, res, next) => {
+    res.status(200).send("Welcome to the official Savvy WebStore API.");
+  });
+
+  router.use("/auth", authorizedRoutes());
+
+  return router;
+};
