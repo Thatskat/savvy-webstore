1a65dec23b44d7319187f4e3aeac374e4995e08e
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-20 18:02:47 +1000

    added json web token to config

--- server/src/configuration/config.js
+++ server/src/configuration/config.js
@@ -2,12 +2,15 @@ module.export = {
   port: process.env.PORT,
   database: {
     accountKey: process.env.GOOGLE_CREDENTIALS,
-    storageBucket: process.env.STORAGE_BUCKET
+    storageBucket: process.env.STORAGE_BUCKET,
   },
   cors: {
     whitelistUrls: {
       development: process.env.CORS_DEVELOPMENT,
-      production: process.env.CORS_PRODUCTION
-    }
-  }
+      production: process.env.CORS_PRODUCTION,
+    },
+  },
+  jsonWebToken: {
+    secret: process.env.JSON_WEB_TOKEN_SECRET,
+  },
 };
