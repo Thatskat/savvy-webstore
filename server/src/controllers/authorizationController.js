1e92492f5250d2a8f74847546ab3eea09bf0ff51
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-20 18:19:46 +1000

    find all users function

--- server/src/controllers/authorizationController.js
+++ server/src/controllers/authorizationController.js
@@ -8,3 +8,30 @@ const {
   convertUserDetailsToJson,
   jsonWebTokenSignUser,
 } = require("../utilities/authorizationServices");
+
+module.exports = {
+  async listAllUsers(req, res, next) {
+    const usersRef = database.collection("users");
+    const snapshot = await usersRef.get();
+
+    if (snapshot.empty) {
+      return next(
+        ErrorsApi.badRequest("You were looking for users that do not exist.")
+      );
+    } else {
+      let users = [];
+      snapshot.forEach((document) => {
+        users.push({
+          email: document.data().email,
+          firstName: document.data().firstName,
+          isAdmin: document.data().isAdmin,
+          lastName: document.data().lastName,
+          password: document.data().password,
+          username: document.data().username,
+        });
+      });
+      res.send(users)
+    }
+  },
+  
+};
