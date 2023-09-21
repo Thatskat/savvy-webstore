d2e786c2dc1bd688184467c6b242ff78136dd91b
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-21 10:33:50 +1000

    added login and register routes

--- server/src/routes/authorizedRoutes.js
+++ server/src/routes/authorizedRoutes.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const authorizationPolicies = require("../policies/authorizationPolicies");
+const authorizationController = require("../controllers/authorizationController");
 
 module.exports = () => {
-    
-}
+  router.get("/users", authorizationController.listAllUsers);
+  router.post(
+    "/register",
+    authorizationPolicies.validateAuthorization,
+    authorizationController.registerUser
+  );
+  router.post(
+    "/login",
+    authorizationPolicies.validateAuthorization,
+    authorizationController.loginUser
+  );
+  return router;
+};
