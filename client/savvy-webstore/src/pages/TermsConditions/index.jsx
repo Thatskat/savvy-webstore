e36566836cc5fc92f0e79da9a5d06e68d16889d6
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-10-01 10:25:34 +1100

    finished html template of terms + conditions

--- client/savvy-webstore/src/pages/TermsConditions/index.jsx
+++ client/savvy-webstore/src/pages/TermsConditions/index.jsx
@@ -63,7 +63,55 @@ const TermsConditionsPage = () => {
         <Link to="/returns">here</Link>, for information on returns, refunds,
         and exchanges.
       </p>
-      
+      <h3>7. Intellectual Property</h3>
+      <ul>
+        <li>
+          All content on our webstore, including text, images, logos, and
+          trademarks, is the intellectual property of Savvy WebStore and is
+          protected by applicable copyright and trademark laws.
+        </li>
+        <li>
+          You may not reproduce, distribute, or use our content without our
+          explicit permission.
+        </li>
+      </ul>
+      <h3>8. User Accounts</h3>
+      <ul>
+        <li>
+          You may be required to create a user account to access certain
+          features of our webstore.
+        </li>
+        <li>
+          You are responsible for maintaining the confidentiality of your
+          account credentials and for all activities that occur under your
+          account.
+        </li>
+      </ul>
+      <h3>9. Limitation of Liability</h3>
+      <ul>
+        <li>
+          Savvy WebStore and its affiliates are not liable for any direct,
+          indirect, incidental, special, or consequential damages resulting from
+          the use of our webstore or products.
+        </li>
+        <li>
+          Our liability is limited to the purchase price of the products you buy
+          from us.
+        </li>
+      </ul>
+      <h3>10. Governing Law</h3>
+      <p>
+        These Terms and Conditions are governed by and construed in accordance
+        with the laws of Victoria, Australia, and any disputes shall be subject
+        to the exclusive jurisdiction of the courts in Victoria, Australia.
+      </p>
+      <h3>11. Changes to Terms</h3>
+      <p>
+        We reserve the right to update or modify these Terms and Conditions at
+        any time. Please check this page periodically for changes. Your
+        continued use of our webstore after any modifications indicates your
+        acceptance of the revised terms.
+      </p>
     </div>
   );
 };
