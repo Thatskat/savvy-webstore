8c5a6eb8dd69a9c125959979f5c85a46877c8092
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-11-01 11:31:52 +1100

    product routes done :)

---
+++ server/src/routes/productRoutes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+const productPolicy = require("../policies/productPolicy");
+const filePolicy = require("../policies/filePolicy");
+const verifyAuth = require("../middleware/verifyAuth");
+const fileServerUpload = require("../middleware/fileServerUpload");
+const productController = require("../controllers/productController");
+
+module.exports = () => {
+  router.get("/", productController.getAllProducts);
+  router.get("/:id", productController.getProductById);
+
+  router.post(
+    "/",
+    [
+      productPolicy.validateProduct,
+      filePolicy.fileExists,
+      filePolicy.fileSize,
+      filePolicy.fileExtension([`.png`, `.jpg`, `.jpeg`, `.webp`]),
+      verifyAuth.auth,
+      fileServerUpload,
+    ],
+    productController.postProduct
+  );
+
+  router.put(
+    "/:id",
+    [
+      productPolicy.validateProduct,
+      filePolicy.fileExists,
+      filePolicy.fileSize,
+      filePolicy.fileExtension([`.png`, `.jpg`, `.jpeg`, `.webp`]),
+      verifyAuth.auth,
+      fileServerUpload,
+    ],
+    productController.putProductById
+  );
+
+  router.delete(
+    "/:id",
+    [verifyAuth.auth, verifyAuth.admin],
+    productController.deleteProductById
+  );
+
+  return router;
+};
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const authorizedRoutes = require("./authorizedRoutes");
-
+const productRoutes = require("./productRoutes");
 
 module.exports = () => {
   router.get("/", (req, res, next) => {
@@ -10,6 +10,8 @@ module.exports = () => {
   });
 
   router.use("/auth", authorizedRoutes());
+
+  router.use("/products", productRoutes());
 
   return router;
 };
