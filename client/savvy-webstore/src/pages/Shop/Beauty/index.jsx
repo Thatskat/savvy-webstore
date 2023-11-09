5912b6adc5fa7f577eabcf7c377bb780aa93d71a
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-11-10 10:12:12 +1100

    got the images working :)

--- client/savvy-webstore/src/pages/Shop/Beauty/index.jsx
+++ client/savvy-webstore/src/pages/Shop/Beauty/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import shopSVG from "../../../assets/savvy-shop.svg";
+import * as styles from "../Shop.css";
 import { Helmet } from "react-helmet";
+import SearchBar from "../../../components/features/SearchBar";
 
 const BeautyPage = () => {
   return (
     <div>
-      <Helmet><title>Shop Beauty | Savvy WebStore</title></Helmet>
+      <Helmet>
+        <title>Shop Beauty | Savvy WebStore</title>
+      </Helmet>
+      <div className={`${styles.shopHeader} ${styles.beautyHeader}`}>
+        <div className="heading">
+          <img src={shopSVG} alt="Shop" />
+          <h1>Health & Beauty</h1>
+        </div>
+      </div>
+      <SearchBar />
+      <p className={styles.itemShow}>
+        Showing <span>#</span> out of #{" "}
+      </p>
+      <div className={styles.itemsGrid}></div>
     </div>
   );
 };
--- client/savvy-webstore/src/pages/Shop/Shop.css.ts
+++ client/savvy-webstore/src/pages/Shop/Shop.css.ts
@@ -2,13 +2,18 @@ import { style, globalStyle } from "@vanilla-extract/css";
 import { root } from "../../styles/themes.css";
 
 export const shopHeader = style({
-  background:
-    "url('https://images.unsplash.com/photo-1682695797221-8164ff1fafc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')",
   height: "40vh",
+ 
+
 });
 
 globalStyle(`${shopHeader} .heading`, {
   padding: `${root.spacing["9x"]} 200px`,
+});
+
+export const beautyHeader = style( {
+  background: "url('/shopHeader/beauty-shop-header.png') no-repeat center",
+  backgroundSize: "cover"
 });
 
 globalStyle(`${shopHeader} .heading img`, {
