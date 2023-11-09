5ecf2cab02dcb3c37dd5f8e55e6eac427c111b61
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-11-09 16:31:06 +1100

    some styling that looks good

--- client/savvy-webstore/src/pages/Stores/Stores.css.ts
+++ client/savvy-webstore/src/pages/Stores/Stores.css.ts
@@ -16,3 +16,49 @@ globalStyle(`${stores} h1`, {
 globalStyle(`${stores} label`, {
   display: "none",
 });
+
+globalStyle(`${stores} input`, {
+  borderRadius: 30,
+  padding: root.spacing["2x"],
+  color: root.colours.secondary,
+  fontFamily: root.fonts.body,
+  borderColor: root.colours.secondary,
+  width: "60%",
+  margin: `${root.spacing["3x"]} auto`,
+});
+
+globalStyle(`${stores} .browseByRegion`, {});
+
+globalStyle(`${stores} .browseByRegion h2`, {
+  fontSize: root.fontSizes["7x"],
+  textAlign: "center",
+});
+
+globalStyle(`${stores} .browseByRegion .stateButton`, {
+  textAlign: "center",
+});
+
+globalStyle(`${stores} .browseByRegion .stateButton button`, {
+  background: "none",
+  border: "none",
+  color: root.colours.secondary,
+  fontSize: root.fontSizes["4x"],
+  margin: `${root.spacing["2x"]}`,
+});
+
+globalStyle(`${stores} .browseByRegion .stateButton button:hover`, {
+  textDecoration: "underline",
+});
+
+globalStyle(`${stores} .browseByRegion .byAlphabet button`, {
+  background: "none",
+  border: "none",
+  color: root.colours.brand,
+  fontSize: root.fontSizes["2x"],
+  margin: `${root.spacing["2x"]}`,
+  textAlign: "center",
+  fontWeight: root.fontWeights.bold,
+});
+globalStyle(`${stores} .browseByRegion .byAlphabet button:hover`, {
+  textDecoration: "underline",
+});
