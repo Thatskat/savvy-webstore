f9fb45925ea3de6de8278d3b6a3c8271cd453907
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-11-10 12:24:21 +1100

    toys done!

--- client/savvy-webstore/src/pages/Shop/Womens/index.jsx
+++ client/savvy-webstore/src/pages/Shop/Womens/index.jsx
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet";
 import productService from "../../../services/productService";
 import ItemCard from "../../../components/common/ItemCard";
 import SearchBar from "../../../components/features/SearchBar";
+import { Link } from "react-router-dom";
 const WomansPage = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -52,7 +53,8 @@ const WomansPage = () => {
         Showing <span>{data.length}</span> out of {data.length}
       </p>
       <div className={styles.itemsGrid}>
-        {data ? data.map((product) => (<ItemCard key={product.id} product={product} link={`/shop/${product.id}`}/>)) : <p>No Items </p>}
+        {data && data.map((product) => (<ItemCard key={product.id} product={product} link={`/shop/${product.id}`}/>))}
+        {data.length === 0 && <p>Looks like there&apos;s no items in this category. Return to <Link to="/shop">All Categories</Link>.</p>}
       </div>
     </div>
   );
