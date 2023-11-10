cbc2ddd0ec36ed6d9fe33c3ebc19ffba92633fdf
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-11-10 13:03:27 +1100

    All store pages done!

--- client/savvy-webstore/src/pages/Shop/Homewares/index.jsx
+++ client/savvy-webstore/src/pages/Shop/Homewares/index.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState, useRef } from "react";
 import shopSVG from "../../../assets/savvy-shop.svg";
 import * as styles from "../Shop.css";
 import { Helmet } from "react-helmet";
 import SearchBar from "../../../components/features/SearchBar";
-
+import productService from "../../../services/productService";
+import { Link } from "react-router-dom";
 const HomewaresPage = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const hasRun = useRef(false);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (hasRun.current === false) {
+      fetchProducts();
+      setLoading(false);
+      return () => {
+        hasRun.current = true;
+      };
+    }
+  }, []);
+
+  async function fetchProducts() {
+    try {
+      const res = await productService.getItemByCategory("Homewares");
+      const data = await res.data;
+      setData(data);
+      console.log(data);
+    } catch (err) {
+      setError(true);
+      console.error(err?.response);
+    }
+  }
+  if (loading) {
+    return <h1 className="loading">Loading...</h1>;
+  }
   return (
     <div>
       <Helmet>
@@ -17,10 +49,11 @@ const HomewaresPage = () => {
       </div>
       <SearchBar />
       <p className={styles.itemShow}>
-        Showing <span>#</span> out of #{" "}
+        Showing <span>{data.length}</span> out of {data.length}
       </p>
       <div className={styles.itemsGrid}>
-
+      {data && data.map((product) => (<ItemCard key={product.id} product={product} link={`/shop/${product.id}`}/>))}
+      {data.length === 0 && <p>Looks like there&apos;s no items in this category. Return to <Link to="/shop">All Categories</Link>.</p>}
       </div>
     </div>
   );
